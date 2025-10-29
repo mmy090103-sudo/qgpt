69e29c667aee0f865a91e66f836c0fed681377b3
mmy090103-sudo <mmy090103@gmail.com>
2025-10-29 19:41:20 +0900

    Update 00_test1.py

--- pages/00_test1.py
+++ pages/00_test1.py
@@ -3,4 +3,4 @@ st.title('첫 사이트 제작하기!!')
 name = st.text_input('이름을 입력해주세요 : ')
 movie = st.selectbox('가장 최근에 본 영화가 뭔가요?:', ['체인소맨','보스','귀멸의칼날','좀비딸'])
 if st.button('인사말 생성') : 
-  st.write(name+'님! +'name'+님이 최근에본 영화는 '+movie+'이군요?! 저도 '+movie+' 봤어요!!')
+  st.write(name+'님! name+'님이 최근에본 영화는 '+movie+'이군요?! 저도 '+movie+' 봤어요!!')
